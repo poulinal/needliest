7bd83ea9719887e94397da00a3dd5369fb9bbb64
poulin-al-northeastern <apoulinpff@gmail.com>
2025-09-22 21:30:33 -0400

    partially tuned

--- init_params.m
+++ init_params.m
@@ -1,38 +1,25 @@
 %%% setup initial parameters
-% 
-initial_pressure = 101325;       % in Pa (1 atm)
-initial_temperature = 300;       % in Kelvin
-valve_opening_fraction = 0.7;    % between 0 and 1
-pipe_diameter = 0.01;            % in meters
-flow_resistance = 1e5;           % in Pa·s/m³ or appropriate units
 
-
+% set basic time sequence
 t = [0, 5];
 t_static = [0];
 
+%intial PID parameters
+k_i = 0.00000000003;
+% k_i = 0.0000000000003;
+% k_p = 0.000000000024;
+k_p = 0.000000000012;
+k_d = 0.000000000002;%0.000000000005;
 
-tarPSI = 100; % target resistance (in psi)
-% tarPa = 
-t_tarPSI = [t,tarPSI]; % target resistance (in psi) but time matrix
+%SP is set point
+% SP = 689475;% 6.89 * 10^5 % 100psi
+SP = 4136854; %4,136,854.368 % 600psi
 
-prevRest = 0; % previous resistance (0 is closed, 1 is open)
-t_prevRest = [t,prevRest];
-
-test_resi = [0, 1];
-t_test_resi = test_resi;
-
-
-% k_i = 0.000005;
-% k_p = 300;
-% k_d = 10;
-k_i = 0;
-k_p = 0.000000000005;
-k_d = 0;
-% SP = 100; %setpoint / target value
-SP = 689475;
+%set zeroed
 Last_error = 0;
 Last_iterm = 0;
 
+%set parameters in time sequence
 t_k_i = [t_static, k_i];
 t_k_p = [t_static, k_p];
 t_k_d = [t_static, k_d];
